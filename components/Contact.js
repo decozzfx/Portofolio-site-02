eb03f497f0087a14d0bd33401759bad28f97f020
decozzfx <m.f.r.ozil@gmail.com>
2022-01-18 12:44:43 +0700

    adding disable feature in contact button , and show fullname to the success modal, add error modal

--- components/Contact.js
+++ components/Contact.js
@@ -10,8 +10,10 @@ export default function Contact(){
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+  const [btnSend, setBtnSend] = useState(false)
 
   let [isOpen, setIsOpen] = useState(false)
+  let [errorIsOpen, setErrorIsOpen] = useState(false)
 
   function closeModal() {
     setIsOpen(false)
@@ -26,10 +28,12 @@ export default function Contact(){
     setEmail('')
     setSubject('')
     setMessage('')
+    closeModal()
   }
 
   function handleSubmit(e){
     e.preventDefault()
+    setBtnSend(true)
     var options = {
       method: 'POST',
       url: process.env.CONTACT_API_URL,
@@ -51,9 +55,11 @@ export default function Contact(){
     axios.request(options).then(function (response) {
       console.log(response.data);
       openModal()
-      resetState()
+      setBtnSend(false)
     }).catch(function (error) {
+      setErrorIsOpen(true)
       console.error(error);
+      setBtnSend(false)
     });
   }
 
@@ -104,7 +110,8 @@ export default function Contact(){
       <textarea name="message" className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" value={message}
             onChange={(e) => {setMessage(e.target.value)}} ></textarea>
       <div className="flex flex-row items-center justify-start">
-        <button type="submit" className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center">
+        
+        <button type="submit" className={`border-green-500px-10 mt-8 py-2 px-6 bg-[#130F49] hover:scale-105 text-gray-50 font-light rounded-md text-lg flex flex-row items-center disabled:opacity-40 ${ btnSend ? 'cursor-not-allowed' : ''} `} disabled={btnSend ? true : false} >
           Send
           <svg width="24" height="24" viewBox="0 0 24 24" className="text-cyan-500 ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z" fill="currentColor" />
@@ -162,7 +169,7 @@ export default function Contact(){
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white border-2 shadow-2xl rounded-2xl ">
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white border-2 shadow-2xl rounded-2xl border-2 border-green-500 ">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-center text-gray-900"
@@ -172,7 +179,7 @@ export default function Contact(){
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
                     Your message has been successfully submitted.
-                    Thank you..
+                    Thank you {fullname}
                   </p>
                 </div>
 
@@ -180,9 +187,71 @@ export default function Contact(){
                   <button
                     type="button"
                     className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={closeModal}
+                    onClick={() => resetState()}
                   >
                     Got it, thanks!
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+      <Transition appear show={errorIsOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white border-2 shadow-2xl rounded-2xl border-2 border-red-500 ">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-center font-semibold text-gray-900"
+                >
+                  Something Error
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500 text-center">
+                    Server is looking busy, please try again..!
+                  </p>
+                </div>
+
+                <div className="mt-4 flex justify-center">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={() => setErrorIsOpen(false)}
+                  >
+                    Try Again!
                   </button>
                 </div>
               </div>
