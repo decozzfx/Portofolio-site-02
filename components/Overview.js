90cced91b8eb6c25931a91a79a45c571b1909fd4
decozzfx <m.f.r.ozil@gmail.com>
2022-09-10 09:01:55 +0700

    fixing overview

--- components/Overview.js
+++ components/Overview.js
@@ -13,25 +13,29 @@ const Overview = () => {
 					<h2 className='mt-14 text-3xl font-semibold '>Quick Education & Experience</h2>
 					<ul className='mt-8'>
 						<li className='mt-4'>
-						<img src="/bullet.svg" className='inline-block mr-4'/>
-						2018 - now : Working at UD. Semulur as Assistant Procurement
+							<img src="/bullet.svg" className='inline-block mr-4' />
+							2022 - now : Working at PT. Infosys Solusi Terpadu as Frontend ReactJS Developer
 						</li>
 						<li className='mt-4'>
-						<img src="/bullet.svg" className='inline-block mr-4'/>
-						2014 - 2017 : College in Politeknik Negeri Madiun
+							<img src="/bullet.svg" className='inline-block mr-4' />
+							2018 - 2022 : UD. Semulur as Assistant Procurement
 						</li>
 						<li className='mt-4'>
-						<img src="/bullet.svg" className='inline-block mr-4'/>
-						2016 : Intern in Bank Mandiri KCP. Madiun
+							<img src="/bullet.svg" className='inline-block mr-4' />
+							2014 - 2017 : College in Politeknik Negeri Madiun
+						</li>
+						<li className='mt-4'>
+							<img src="/bullet.svg" className='inline-block mr-4' />
+							2016 : Intern in Bank Mandiri KCP. Madiun
 						</li>
 					</ul>
 				</div>
-			<div className="w-6/12">
-				<div className="flex items-center justify-center mt-12 md:pb-0 pb-10">
-					<img src="/potrait-1-1.png" className='rounded-xl shadow-2xl shadow-white/80 hover:scale-105' />
+				<div className="w-6/12">
+					<div className="flex items-center justify-center mt-12 md:pb-0 pb-10">
+						<img src="/potrait-1-1.png" className='rounded-xl shadow-2xl shadow-white/80 hover:scale-105' />
+					</div>
 				</div>
 			</div>
-		</div>
 		</section>
 	)
 }
